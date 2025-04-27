93c5ab398407f55428824a808417115868690afc
Kevin Slawinski <kevin.slawinski@gmail.com>
2025-04-26 22:14:49 -0400

    move media and css into place, clean up default components

--- about-me/src/app/app.component.spec.ts
+++ about-me/src/app/app.component.spec.ts
@@ -14,16 +14,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'about-me' title`, () => {
+  it(`should have the 'About | Kevin Slawinski' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('about-me');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, about-me');
+    expect(app.title).toEqual('About | Kevin Slawinski');
   });
 });
--- about-me/src/app/app.component.ts
+++ about-me/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, HeaderComponent],
+  template: `
+    <app-header></app-header>
+    <router-outlet />
+  `,
+  styles: ``,
 })
 export class AppComponent {
-  title = 'about-me';
+  title = 'About | Kevin Slawinski';
 }
