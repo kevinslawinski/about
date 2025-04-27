bb2d89a9e91598c858007c81ced85a70458e7265
Kevin Slawinski <kevin.slawinski@gmail.com>
2025-04-26 23:59:49 -0400

    make basic tests pass

--- about-me/src/app/components/not-found/not-found.component.spec.ts
+++ about-me/src/app/components/not-found/not-found.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { ActivatedRoute } from '@angular/router';
 import { NotFoundComponent } from './not-found.component';
 
 describe('NotFoundComponent', () => {
@@ -9,6 +10,7 @@ describe('NotFoundComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [NotFoundComponent],
+      providers: [{ provide: ActivatedRoute, useValue: {} }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NotFoundComponent);
